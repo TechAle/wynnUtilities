e0f72f929b2c345fc6277ed2e088013b7a1b00f6
TechAle <alessandro.condello.email@gmail.com>
2022-09-01 12:07:08 +0200

    t

--- crafter/creations.py
+++ crafter/creations.py
@@ -1,5 +1,6 @@
-from api import WynnPy
+import json
 
+from api import WynnPy
 
 def createDatalist():
     wynnApi = WynnPy.wynnPy()
@@ -9,7 +10,6 @@ def createDatalist():
         for modifier in ing["statuses"]:
             if not modifiers.__contains__(modifier):
                 modifiers.append(modifier)
-    b = 0
     header = '<datalist id="modifiers">'
     body = ''
     for modifier in modifiers:
@@ -17,5 +17,18 @@ def createDatalist():
     footer = '</datalist>'
     print(header + body + footer)
 
-
-createDatalist()
+def createDataset():
+    wynnApi = WynnPy.wynnPy()
+    ings = wynnApi.getIngridients()
+    output = {}
+    for ing in ings["ingredients"]:
+        output[ing["name"]] = {
+            "tier": ing["tier"],
+            "level": ing["level"],
+            "professions": ing["professions"],
+            "statuses": ing["statuses"],
+            "itemModifiers": ing["itemModifiers"],
+            "ingredientModifiers": ing["ingredientModifiers"]
+        }
+    with open('./crafter/dataset/ings.json', 'w') as fp:
+        json.dump(output, fp)
